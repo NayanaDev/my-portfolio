a1aab30aad823b24d325ead3799be3f509838fb1
Nayana <naina1290@gmail.com>
2025-09-09 14:11:44 +0530

    initial project setup

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Tells Tailwind to scan these files for classes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
